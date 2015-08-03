1aceaad27a86b99cb5e9af0198469c6de5a0a31e
bungubbang <bub4419>
2015-08-03 10:08:10 +0900

    GCM 버그 수정

--- app/src/main/java/com/fevi/fadong/GCMBroadcastReceiver.java
+++ app/src/main/java/com/fevi/fadong/GCMBroadcastReceiver.java
@@ -1,12 +1,9 @@
 package com.fevi.fadong;
 
-import android.app.Activity;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.support.v4.content.WakefulBroadcastReceiver;
-
-import com.fevi.fadong.GCMIntentService;
 
 public class GCMBroadcastReceiver extends WakefulBroadcastReceiver {
 
@@ -17,7 +14,7 @@ public class GCMBroadcastReceiver extends WakefulBroadcastReceiver {
                 GCMIntentService.class.getName());
         // Start the service, keeping the device awake while it is launching.
         startWakefulService(context, (intent.setComponent(comp)));
-        setResultCode(Activity.RESULT_OK);
+//        setResultCode(Activity.RESULT_OK);
 	}
 
 }
